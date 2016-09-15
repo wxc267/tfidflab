4400d060a81081e8e2fa892797b2491571c76ad5
wxc267 <wxc267@gmail.com>
2016-09-14 23:44:19 -0400

    add file

---
+++ datamininglab1/src/datamininglab1/UtilClass.java
@@ -0,0 +1,25 @@
+package datamininglab1;
+
+
+import java.util.Map;
+
+public class UtilClass {
+	public static String GetThreeDigitsNumber(int number)
+	{
+		String result="000";
+		if(number<10&&number>=0)
+		{
+			result="00"+String.valueOf(number);
+		}
+		else if(number>=10&&number<100)
+		{
+			result="0"+String.valueOf(number);
+		}
+		else
+		{
+			result=String.valueOf(number);
+		}
+		return result;
+	}
+
+}
